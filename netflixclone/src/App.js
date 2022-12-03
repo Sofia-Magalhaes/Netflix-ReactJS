58d6817edca611f23bf97316c4e49971de6b4581
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2022-12-03 17:00:09 -0300

    Ajustando css inicial e mostrando os posters

--- netflixclone/src/App.js
+++ netflixclone/src/App.js
@@ -1,10 +1,29 @@
 /* eslint-disable import/no-anonymous-default-export */
-import React from "react";
+import React, { useEffect, useState } from "react";
+import './App.css';
+import Tmdb from "./Tmdb";
+import MovieRow from "./components/MovieRow";
 
 export default () => {
+  const [movieList, setMovieList] = useState([]);
+
+
+  useEffect(() => {
+    const loadAll = async () => {
+      // Pegando a lista TOTAL
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+    }
+    loadAll();
+  }, []);
+
   return (
-    <div>
-      oi
+    <div className="page">
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items}/>
+        ))}
+      </section>
     </div>
   );
 }
