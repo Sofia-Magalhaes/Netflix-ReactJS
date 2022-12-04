2a1e82f0b6aebb8b39aaa7703976d26300b791e7
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2022-12-03 21:08:03 -0300

    header

--- netflixclone/src/App.js
+++ netflixclone/src/App.js
@@ -22,16 +22,26 @@ export default () => {
       let originals = list.filter(i => i.slug === 'originals');
       let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
       let chosen = originals[0].items.results[randomChosen];
-      let chosenInfo = await Tmdb.getMovieInfo(chosen.id,'tv');
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
       setFeaturedData(chosenInfo);
-      
+
     }
     loadAll();
   }, []);
 
-  useEffect(()=>{
-    
-  },[]);
+  useEffect(() => {
+    const scrolListner = () => {
+      if (window.scrollY > 10) {
+        setBlackHeader(true);
+      } else {
+        setBlackHeader(false);
+      }
+    }
+    window.addEventListener('scroll', scrolListner);
+    return () => {
+      window.removeEventListener('scroll', scrolListner);
+    }
+  }, []);
 
   return (
     <div className="page">
