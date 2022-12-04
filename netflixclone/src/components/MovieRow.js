fe179a5a084b8d76bf678c3fe5d134d67f10b93a
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2022-12-04 18:38:44 -0300

    scroll dos dois lados

--- netflixclone/src/components/MovieRow.js
+++ netflixclone/src/components/MovieRow.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-anonymous-default-export */
-import React, {useState} from "react";
+import React, { useState } from "react";
 import './MovieRow.css';
 
 
@@ -8,11 +8,20 @@ export default ({ title, items }) => {
     const [scrollX, setScrollX] = useState(-400);
 
     const handleLeftArrow = () => {
-        
+        let x = scrollX + Math.round(window.innerWidth / 2);
+        if (x > 0) {
+            x = 0;
+        }
+        setScrollX(x);
     }
 
     const handleRightArrow = () => {
-        
+        let x = scrollX - Math.round(window.innerWidth / 2);
+        let listW = items.results.length * 150;
+        if((window.innerWidth - listW > x)){
+            x = (window.innerWidth - listW) - 60;
+        }
+        setScrollX(x);
     }
 
     return (
@@ -20,12 +29,12 @@ export default ({ title, items }) => {
             <h2>{title}</h2>
             <div className="movieRow--left" onClick={handleLeftArrow}>
                 <p style={{ fontSize: 30 }}>
-                    <span role="img" arial-label="setaEsquerda">➡️</span>
+                    <span role="img" arial-label="setaDireita">⬅️</span>
                 </p>
             </div>
             <div className="movieRow--right" onClick={handleRightArrow}>
                 <p style={{ fontSize: 30 }}>
-                    <span role="img" arial-label="setaDireita">⬅️</span>
+                    <span role="img" arial-label="setaEsquerda">➡️</span>
                 </p>
             </div>
 
